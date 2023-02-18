30f1ac425362c4521cb47a64d5124dea0fd3d99c
Jan Peter <jcp22@ic.ac.uk>
2023-02-18 19:36:59 +0000

    adapted backend file to segment masks

--- 01_HPC/rf_download.py
+++ 01_HPC/rf_download.py
@@ -1,4 +1,5 @@
 from roboflow import Roboflow
+
 rf = Roboflow(api_key="GjP8E52qNp1AUWdM04dK")
 project = rf.workspace("").project("foodsnap_data")
 dataset = project.version(1).download("yolov8")
--- knowledge/backend_integration.py
+++ knowledge/backend_integration.py
@@ -1,7 +1,9 @@
+#file meant for backend
+
 from ultralytics import YOLO
 import numpy as np
 
-def detect_food_items(input):
+def detect_food_items(Path):
     """
     Generate food class prediction and item coordinates
     using YOLO model.
@@ -17,30 +19,22 @@ def detect_food_items(input):
 
     # generate prediction based on input image
     # classes: classes included in search (0 = bowl, 1 = omelette, 2 = plate)
-    results = model.predict(source=input)
+    results = model.predict(source=input, classes=[1])
+
+    # get normalized width and height
+    pos_tensor = results[0].boxes.xywhn
 
     # get identified classes
     cls_tensor = results[0].boxes.cls
+
+    # convert to numpy array and delete coordinates of box
+    pos_np = pos_tensor.detach().numpy()
+    dims = np.delete(pos_np, [0, 1], axis=1)
 
     # create numpy label array
     cls_np = np.expand_dims(cls_tensor.detach().numpy(), axis=0)
     names_dict = model.names
     to_np = np.vectorize(lambda i: names_dict[i])
-    labels = np.asarray(to_np(cls_np))
+    labels = to_np(cls_np)
 
-    # extract mask element
-    mask_data = results[0].masks.data # raw masks tensor (N, H, W)
-
-    # create empty np array for object areas
-    area_np = np.empty(shape=labels.size, dtype=float)
-
-    # iterate through elements in tensor and extract mask size
-    for i in range(mask_data.shape[0]):
-        mask_zero = (mask_data[i] == 0).sum()
-        mask_one = (mask_data[i] == 1).sum()
-        area_tensor = mask_one / (mask_zero + mask_one)
-        area_np[i] = area_tensor.detach().numpy()
-
-    return area_np, labels
-
-print(detect_food_items("./02_local_testing/omelette_1.jpeg"))
+    return dims, labels
